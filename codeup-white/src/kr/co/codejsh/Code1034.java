ee4c41d830926b327817f4df832af4486f870dfa
jinseonghee <wlstjdgml0610@gmail.com>
2021-04-21 22:54:43 +0900

    code-up 1034

---
+++ codeup-white/src/kr/co/codejsh/Code1034.java
@@ -0,0 +1,12 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1034 {
+
+    public static void main(String[] args) {
+
+        System.out.printf(String.format("%o\n", new Scanner(System.in).nextInt()));
+    }
+
+}
