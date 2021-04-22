6cf1fcb922d9da8c076ec037c388b2bebf929b73
jinseonghee <wlstjdgml0610@gmail.com>
2021-04-22 20:46:29 +0900

    code-up 1035

---
+++ codeup-white/src/kr/co/codejsh/Code1035.java
@@ -0,0 +1,17 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1035 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String x = scan.nextLine();
+        int y = Integer.valueOf(x, 16);
+        System.out.printf("%d\n",y);
+
+
+    }
+}
