3ac2e066eaa1227ecea12f0aca37d1f3b95929f2
jinseonghee <wlstjdgml0610@gmail.com>
2021-04-23 21:28:09 +0900

    code-up 1036

---
+++ codeup-white/src/kr/co/codejsh/Code1036.java
@@ -0,0 +1,17 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1036 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        char a = scan.next().charAt(0);
+
+        System.out.println((int)a);
+
+
+    }
+}
