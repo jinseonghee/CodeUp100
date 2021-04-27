eaeaea51bf8ab33f9a0025e65e64f30ec55602d0
jinseonghee <wlstjdgml0610@gmail.com>
2021-04-27 23:31:29 +0900

    code-up 1040

---
+++ codeup-white/src/kr/co/codejsh/Code1040.java
@@ -0,0 +1,14 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1040 {
+
+    public static void main(String[] args) {
+        /*Scanner scan = new Scanner(System.in);
+        long a = scan.nextLong();
+        System.out.printf("%d\n",(-a));*/
+
+        System.out.println(-(new Scanner(System.in).nextInt()));
+    }
+}
