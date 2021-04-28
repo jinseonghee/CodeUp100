8fcb2d3e214fe6140d83ee33330e4302d8fca793
jinseonghee <wlstjdgml0610@gmail.com>
2021-04-28 23:50:38 +0900

    code-up 1041

---
+++ codeup-white/src/kr/co/codejsh/Code1041.java
@@ -0,0 +1,16 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1041 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        char a = scan.next().charAt(0);
+        System.out.println((char)(a+1));
+
+
+    }
+
+}
