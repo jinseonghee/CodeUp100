5da8125ff3672a43dfb38bafce0af26e8af6ab11
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-02 23:16:15 +0900

    code-up 1044

---
+++ codeup-white/src/kr/co/codejsh/Code1044.java
@@ -0,0 +1,12 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1044 {
+
+    public static void main(String[] args) {
+
+        Long a = new Scanner(System.in).nextLong();
+        System.out.println(++a);
+    }
+}
