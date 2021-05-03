f9c41dace9fb49e5e6ae306902420a755a602980
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-03 23:14:32 +0900

    code-up 1045

---
+++ codeup-white/src/kr/co/codejsh/Codeup1045.java
@@ -0,0 +1,20 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Codeup1045 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d\n", a+b);
+        System.out.printf("%d\n", a-b);
+        System.out.printf("%d\n", a*b);
+        System.out.printf("%d\n", a/b);
+        System.out.printf("%d\n", a%b);
+        double A = (double) a;
+        System.out.printf("%.2f\n", A/b);
+    }
+}
+
+
