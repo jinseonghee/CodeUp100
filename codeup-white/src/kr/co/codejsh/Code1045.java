84b02bb678b0350ce80525a0e0fbbd80ed43fcfb
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-04 18:31:16 +0900

    code-up 1046

--- codeup-white/src/kr/co/codejsh/Code1045.java
+++ codeup-white/src/kr/co/codejsh/Code1045.java
@@ -2,7 +2,7 @@ package kr.co.codejsh;
 
 import java.util.Scanner;
 
-public class Codeup1045 {
+public class Code1045 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
