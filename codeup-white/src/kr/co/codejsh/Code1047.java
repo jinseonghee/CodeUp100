5d1ee914123e286e62d0624a85e83153337a5697
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-05 22:58:51 +0900

    code-up 1047

---
+++ codeup-white/src/kr/co/codejsh/Code1047.java
@@ -0,0 +1,12 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1047 {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Scanner(System.in).nextLong()<<1);
+    }
+
+}
