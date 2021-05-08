fcb64804520ace337a2b5502c6083a89f15d4cca
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-09 02:16:30 +0900

    code-up 1048 refactoring

--- codeup-white/src/kr/co/codejsh/Code1048.java
+++ codeup-white/src/kr/co/codejsh/Code1048.java
@@ -1,28 +1,11 @@
 package kr.co.codejsh;
 
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Code1048 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String nums = sc.nextLine();
-
-        StringTokenizer st = new StringTokenizer(nums, " ");
-
-        String arr[] = new String[2];
-
-        int i = 0;
-        while (st.hasMoreTokens()) {
-            arr[i] = st.nextToken();
-            i += 1;
-        }
-
-        int a = Integer.parseInt(arr[0]);
-        int b = Integer.parseInt(arr[1]);
-
-        System.out.println(a << b);
+        String inputData[] = new Scanner(System.in).nextLine().split(" ");
+        System.out.println(Integer.parseInt(inputData[0]) * (int) Math.pow(2, Integer.parseInt(inputData[1])));
     }
 }
