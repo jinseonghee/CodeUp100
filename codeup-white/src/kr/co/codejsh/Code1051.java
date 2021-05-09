c4262fe333fd6f865183b89cf039fb639e7ae06a
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-09 19:54:41 +0900

    code-up 1051

---
+++ codeup-white/src/kr/co/codejsh/Code1051.java
@@ -0,0 +1,25 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1051 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        long inputData[] = new long[2];
+        inputData[0] = scan.nextLong();
+        inputData[1] = scan.nextLong();
+
+        System.out.println(getResultValue(inputData));
+    }
+    private static String getResultValue(long[] inputData) {
+        if(isCompareToValue(inputData))
+        return "1";
+            return "0";
+    }
+    private static boolean isCompareToValue(long[] inputData){
+            return (inputData[0] <= inputData[1]);
+    }
+}
+
