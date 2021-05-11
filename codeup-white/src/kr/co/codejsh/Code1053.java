9e9ec7ff36ed22c4d3d89607a5c2e83d44b3a195
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-11 23:45:43 +0900

    code-up 1053

---
+++ codeup-white/src/kr/co/codejsh/Code1053.java
@@ -0,0 +1,18 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1053 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int inputData = scan.nextInt();
+
+        if (inputData == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+
+    }
+}
