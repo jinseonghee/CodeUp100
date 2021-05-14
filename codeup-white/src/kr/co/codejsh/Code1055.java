fedbc62429bed313793ccf91b9495331b6390578
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-14 23:44:14 +0900

    code-up 1055 refactoring

--- codeup-white/src/kr/co/codejsh/Code1055.java
+++ codeup-white/src/kr/co/codejsh/Code1055.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Code1055 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        String inputData[] = new Scanner(System.in).nextLine().split(" ");
+        System.out.println(getResultValue(inputData));
+    }
 
-        if (a == 1 || b == 1)
-            System.out.println(1);
-        else
-            System.out.println(0);
+    public static String getResultValue(String[] inputData) {
+        if (isCompareToVatlue(inputData))
+            return "1";
+        return "0";
+    }
+
+    public static boolean isCompareToVatlue(String[] inputData) {
+        return (inputData[0].equals("1") || inputData[1].equals("1"));
     }
 }
-
