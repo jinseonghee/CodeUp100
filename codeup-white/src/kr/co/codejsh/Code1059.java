0ef7a0b28c1557343b143544797c59cc729220ef
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-17 23:49:57 +0900

    code-up 1059

---
+++ codeup-white/src/kr/co/codejsh/Code1059.java
@@ -0,0 +1,12 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1059 {
+
+    public static void main(String[] args) {
+
+        System.out.println(~(new Scanner(System.in).nextLong()));
+
+    }
+}
