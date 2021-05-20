66d387963209602f2f7ff536b9431e0154ac4343
jinseonghee <wlstjdgml0610@gmail.com>
2021-05-20 22:10:43 +0900

    code-up 1062

---
+++ codeup-white/src/kr/co/codejsh/Code1062.java
@@ -0,0 +1,12 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1062 {
+
+    public static void main(String[] args) {
+
+        String inputData[] = new Scanner(System.in).nextLine().split(" ");
+        System.out.println(Long.parseLong(inputData[0]) ^ Long.parseLong(inputData[1]));
+    }
+}
