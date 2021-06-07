13ffc462ce910108926453b169fd0626074b2582
jinseonghee <wlstjdgml0610@gmail.com>
2021-06-07 23:11:31 +0900

    code-up 1074

---
+++ codeup-white/src/kr/co/codejsh/Code1074.java
@@ -0,0 +1,15 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1074 {
+
+    public static void main(String[] args) {
+
+        int inputData = new Scanner(System.in).nextInt();
+
+        while (inputData > 0) {
+            System.out.println(inputData--);
+        }
+    }
+}
