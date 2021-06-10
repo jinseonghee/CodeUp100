a37ac802706fc89886e987e2fa36b2d63415ff36
jinseonghee <wlstjdgml0610@gmail.com>
2021-06-10 12:40:07 +0900

    code-up 1077

---
+++ codeup-white/src/kr/co/codejsh/Code1077.java
@@ -0,0 +1,16 @@
+package kr.co.codejsh;
+
+import java.util.Scanner;
+
+public class Code1077 {
+
+    public static void main(String[] args) {
+
+       int inputData = new Scanner(System.in).nextInt();
+
+        for (int i = 0; i <= inputData; i++ ) {
+            System.out.println(i);
+        }
+    }
+}
+
