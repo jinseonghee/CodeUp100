d1654c0911edad212a9b622cdc57c2c9582d3255
jinseonghee <wlstjdgml0610@gmail.com>
2021-09-25 22:07:52 +0900

    code-up 1224 modified

--- codeup-white/src/kr/co/codejsh/Code1222.java
+++ codeup-white/src/kr/co/codejsh/Code1222.java
@@ -1,2 +1,34 @@
-package kr.co.codejsh;public class Code1222 {
+package kr.co.codejsh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Code1222 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int time = Integer.parseInt(st.nextToken());
+        int goal1 = Integer.parseInt(st.nextToken());
+        int goal2 = Integer.parseInt(st.nextToken());
+
+        int addGoal = 0;
+
+        if ((90-time)%5==0) {
+            addGoal = (90-time)/5;
+        } else {
+            addGoal = (90-time)/5+1;
+        }
+
+        sb.append(addGoal>(goal2-goal1) ? "win" : addGoal<(goal2-goal1) ? "lose" : "same");
+
+        System.out.println(sb);
+        br.close();
+    }
 }
+
--- codeup-white/src/kr/co/codejsh/Code1224.java
+++ codeup-white/src/kr/co/codejsh/Code1224.java
@@ -1,2 +1,30 @@
-package kr.co.codejsh;public class Code1224 {
+package kr.co.codejsh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Code1224 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        double first = (double)a/b;
+        double second = (double)c/d;
+
+        sb.append(first>second ? ">" : first<second ? "<" : "=");
+
+        System.out.println(sb);
+        br.close();
+    }
 }
+
