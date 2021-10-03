52403f3517f7614f9de6bd5b3e52268c473a2d8a
jinseonghee <wlstjdgml0610@gmail.com>
2021-10-03 23:20:28 +0900

    code-up 1251

---
+++ codeup-white/src/kr/co/codejsh/Code1251.java
@@ -0,0 +1,14 @@
+package kr.co.codejsh;
+
+public class Code1251 {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=1; i<=100; i++) {
+            sb.append(i+" ");
+        }
+        System.out.println(sb);
+    }
+}
+
